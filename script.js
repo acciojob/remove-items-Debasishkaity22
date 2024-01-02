cf880f5b109563b93d2732c44b60dc2af2b5eee7
Debasishkaity22 <139854092+Debasishkaity22@users.noreply.github.com>
2024-01-02 20:10:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+function remove(){
+    let select=document.querySelector("#colorSelect");
+    let selectIndex=select.selectedIndex;
+   select[selectIndex].remove();
+}
+let input=document.querySelector("form>input");
+input.addEventListener("click",remove);
